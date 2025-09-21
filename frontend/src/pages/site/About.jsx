ff7730d2e2a3e2c9f4285c49acb0eebaae462837
Sijal Manandhar <sijal.m06@gmail.com>
2025-09-22 01:03:53 +0545

    Update about page and README.md content

--- frontend/src/pages/site/About.jsx
+++ frontend/src/pages/site/About.jsx
@@ -2,7 +2,7 @@ import aboutImage from "@/assets/images/about-image.jpg";
 import useTitle from "@/hooks/useTitle.js";
 
 const About = () => {
-  useTitle("About Us | ShopSwift");
+  useTitle("About Project | ShopSwift");
   return (
     <>
       <section>
@@ -13,29 +13,17 @@ const About = () => {
             </div>
 
             <div className="space-y-3 lg:space-y-5 w-full max-w-md md:max-w-lg">
-              <h1
-                className="text-3xl lg:text-5xl dark:text-light text-dark-secondary font-extrabold mb-4"
-              >
-                About Us
+              <h1 className="text-3xl lg:text-5xl dark:text-light text-dark-secondary font-extrabold mb-4">
+                Project SwiftShop
               </h1>
               <div className="font-normal text-base lg:text-lg dark:text-light space-y-3">
                 <p>
-                  Welcome to SwiftShop, a personal project showcasing a simple and
-                  elegant e-commerce website built using MERN Stack. This project serves
-                  as a learning exercise to explore web development fundamentals by
-                  creating a functional online store with product listings, user
-                  authentication, shopping cart, and order management.
-                </p>
-                <p>
-                  Explore the {" "}
-                  <a
-                    href="https://github.com/sam4web/shopswift-frontend"
-                    className="text-primary font-medium hover:underline"
-                    target="_blank"
-                  >
-                    source code
-                  </a>{" "}
-                  on GitHub to see the implementation details.
+                  Welcome to <b>SwiftShop</b>, a modern e-commerce platform developed as a <b>practice project</b> to
+                  demonstrate full-stack web development skills. Built using the <b>MERN Stack</b>, it offers a seamless
+                  online shopping experience, showcasing robust functionalities like <b>user authentication</b>,{" "}
+                  <b>product listings</b>, <b>shopping cart management</b>, and <b>order processing</b>. The entire site
+                  is designed with a <b>responsive design</b>, ensuring a smooth experience across both desktop and
+                  mobile devices.
                 </p>
               </div>
             </div>
@@ -45,30 +33,32 @@ const About = () => {
 
       <section className="bg-light dark:bg-dark-primary py-1">
         <div className="section-container">
-          <h3 className="text-3xl lg:text-4xl text-gray font-extrabold dark:text-light">
-            Our Story
-          </h3>
+          <h3 className="text-3xl lg:text-4xl text-gray font-extrabold dark:text-light">Technologies Used</h3>
           <hr className="line-break" />
 
-          <div
-            className="space-y-4 lg:space-y-7 mt-6 font-normal dark:text-light text-base lg:text-lg"
-          >
+          <div className="space-y-4 lg:space-y-7 mt-6 font-normal dark:text-light text-base lg:text-lg">
             <p className="float-left max-w-2xl">
-              Ducimus non recusandae, voluptas itaque tempore, reprehenderit
-              laudantium amet, iure neque nostrum animi architecto nihil aperiam.
-              Voluptatibus culpa cum autem aspernatur. Veritatis, vel ullam cupiditate
-              similique quo, iste quia accusamus eligendi vero dolorem in, unde aut
-              consequatur. Magni maiores facere earum sapiente iusto alias aspernatur
-              dolor illum dicta? Tempore odio odit cum pariatur sed. Voluptatem,
-              molestias ipsum. Amet, ab.
+              SwiftShop&apos;s backend is powered by <b>Node.js</b> and <b>Express.js</b>, providing a secure and
+              scalable API. We use <b>MongoDB</b> for efficient data storage, with <b>Mongoose</b> for seamless
+              interaction, and <b>JWT (JSON Web Tokens)</b> for secure authentication. This robust foundation supports
+              crucial features like <b>product management</b> (creating, updating, deleting items) and real-time{" "}
+              <b>order processing</b>. The frontend, built with <b>React.js</b> and <b>Redux</b>, delivers a dynamic and
+              interactive user interface, styled efficiently with <b>TailwindCSS</b>.
             </p>
             <div className="clear-both"></div>
 
             <p className="float-right max-w-2xl">
-              Deleniti voluptas consequatur iusto, ut expedita debitis, ratione
-              asperiores molestiae praesentium voluptates nobis dolores, fugiat nisi
-              unde nulla soluta iste. Modi cumque pariatur voluptatibus, magni maxime
-              optio dolorem ipsum distinctio sint, odit illum
+              This project serves as a practical exercise in building a complete e-commerce ecosystem, highlighting
+              skills in modern web technologies and best practices. Explore the on GitHub to see the implementation
+              details. You can explore the source code for this project on
+              <a
+                href="https://github.com/sam4web/shopswift-frontend"
+                className="text-primary font-medium hover:underline"
+                target="_blank"
+              >
+                GitHub
+              </a>
+              .
             </p>
             <div className="clear-both"></div>
           </div>
